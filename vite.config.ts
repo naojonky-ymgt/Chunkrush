1c8aa0007947029b6e5c5f295aaac2cd8a9a62d7
naojonky-ymgt <naojonky@gmail.com>
2026-02-22 16:48:29 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+      base: '/Chunkrush/', // ここにGitHubのリポジトリ名を入れる
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
